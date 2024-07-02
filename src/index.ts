62522004e0374bb6693102ec6e7ef3d532966908
Clover <clover.hsc@gmail.com>
2024-07-02 23:46:35 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hello')
+  .post('/hi', () => 'Hi')
+  .route('SEARCH', '/search', () => 'Search')
+  .onError(({ code }) => {
+    if (code === '400')
+      return 'Route not found :('
+  })
+  .listen(3000);
+
+
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+app.handle(new Request('http://localhost:3000/')).then(console.log)
